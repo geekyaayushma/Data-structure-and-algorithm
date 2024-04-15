12a311bef014c9c40b81f0d9f64a3c3e3548d20f
geekyaayushma <128393956+geekyaayushma@users.noreply.github.com>
2024-04-15 06:51:29 +0545

    Add files via upload

---
+++ Linked List/Check linked list is sorted or not.c
@@ -0,0 +1,51 @@
+//To check linked list is sorted or not.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void Display(struct Node *p) {
+    while (p != NULL) {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+void create(int A[], int n) {
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++) {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+int isSorted(struct Node *p) {
+    int x = -65536;
+    while (p != NULL) {
+        if (p->data < x)
+            return 0;
+        x = p->data;
+        p = p->next;
+    }
+    return 1;
+}
+
+int main() {
+    int A[] = {10, 20, 30, 40, 50};
+    create(A, 5);
+    printf("Linked list is sorted\n", isSorted(first));
+    return 0;
+}
+//The time complexity is O(n).
